8f295dbd5369c80bcfb7e9824d5e262f49e8285f
Gabriele Oliveira <oliveiragabrielesouza@gmail.com>
2024-05-25 21:33:36 -0300

    Update: formatando arquivo

--- analitics/script_sql_analytics.sql
+++ analitics/script_sql_analytics.sql
@@ -18,18 +18,17 @@ FROM tb_dados_api_openweathermap;
 SELECT 
     DATE_FORMAT(dt, '%H') AS hora, 
     AVG(
-	        CASE 
-			            WHEN visibility > 10000 THEN 100.0
-					            ELSE (visibility / 10000.0) * 100.0 
-							        END
-								    ) AS visibilidade_media_porcentagem
-								FROM tb_dados_api_openweathermap
-								GROUP BY DATE_FORMAT(dt, '%H')
-								ORDER BY hora;
+        CASE 
+            WHEN visibility > 10000 THEN 100.0
+            ELSE (visibility / 10000.0) * 100.0 
+        END
+    ) AS visibilidade_media_porcentagem
+FROM tb_dados_api_openweathermap
+GROUP BY DATE_FORMAT(dt, '%H')
+ORDER BY hora;
 
 
-								SELECT DATE_FORMAT(dt, '%H') AS horas, CAST(AVG(wind.speed) AS DECIMAL(10,2)) AS media_velocidade, CAST(AVG(wind.deg) AS INT) AS media_kilometros_rodado
-								FROM tb_dados_api_openweathermap
-								GROUP BY DATE_FORMAT(dt, '%H')
-								ORDER BY horas;
-
+SELECT DATE_FORMAT(dt, '%H') AS horas, CAST(AVG(wind.speed) AS DECIMAL(10,2)) AS media_velocidade, CAST(AVG(wind.deg) AS INT) AS media_kilometros_rodado
+FROM tb_dados_api_openweathermap
+GROUP BY DATE_FORMAT(dt, '%H')
+ORDER BY horas;
